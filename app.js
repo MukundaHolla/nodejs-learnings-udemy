b2d0d28ffb5cb59f56dc6b5b61445bdb2a354c5b
MukundaHolla <kanda05holla@gmail.com>
2019-08-27 22:51:01 +0530

    redirecting requests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Core modules -
 // http, https, fs, path, os 
 const http = require('http');
-
+const fs = require('fs');
 // function requestListener(req, res) {
 // can call this inside createServer()
 // }
@@ -10,13 +10,20 @@ const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers)
     // process.exit(); // node event cycle and event loop
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html><head>Node testing! Sending response</head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
         res.write('</html>');
         return res.end();
     }
-
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Duummy text')
+        //res.writeHead(302, {})
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html><head>Node testing! Sending response</head></html>')
     res.end();
