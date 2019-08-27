466af5538d2915b16ac1f84873e3c247803e38b9
MukundaHolla <kanda05holla@gmail.com>
2019-08-27 22:29:06 +0530

    sending response

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const http = require('http');
 // }
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+    // process.exit(); // node event cycle and event loop
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head>Node testing! Sending response</head></html>')
+    res.end();
 })
 
 server.listen(2000);
