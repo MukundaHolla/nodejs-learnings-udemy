47fec020632658a5966e6c32c0c33650440749ef
MukundaHolla <kanda05holla@gmail.com>
2019-08-29 11:22:22 +0530

    parsing request body

--- app.js
+++ app.js
@@ -18,7 +18,18 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'Duummy text')
+        // register event listener for incoming stream of data
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        
         //res.writeHead(302, {})
         res.statusCode = 302;
         res.setHeader('Location', '/');
