35585239b592e60d334341b48dc76cf060fbba67
MukundaHolla <kanda05holla@gmail.com>
2019-08-29 14:42:44 +0530

    installing nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const http = require("http");
 // function requestListener(req, res) {
 // can call this inside createServer()
-// }
+// } //old method
 const routes = require('./routes');
 
 const server = http.createServer(routes);
